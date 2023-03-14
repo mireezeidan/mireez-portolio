8b8f6b301a56b0c77ba4916b954c8cb64787211f
Miree Zeidan <mireezeidan@gmail.com>
2023-03-14 18:31:12 -0500

    created components, imported react files

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>Created by Miree Zeidan</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>Miree Zeidan Porfolio</h1>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
