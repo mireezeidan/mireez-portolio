59b72a487624ace96987f761de40bdd8364a36f5
Miree Zeidan <mireezeidan@gmail.com>
2023-03-16 18:25:17 -0500

    added routes to about,contact, and project buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Project from "./components/Project";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Project />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
       <h1>Miree Zeidan Porfolio</h1>
       <nav>
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
     </header>
   );
