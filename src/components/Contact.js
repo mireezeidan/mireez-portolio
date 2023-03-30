390bca38a599ad8583c621842339154ff8bc6ddb
Miree Zeidan <mireezeidan@gmail.com>
2023-03-30 14:11:33 -0500

    finished contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,34 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 import "./Contact.css";
 import Github from "../images/github-logo.png";
+import Linkedin from "../images/linkedin-logo.png";
+import Resume from "../images/resume.png";
+
 function Contact() {
   return (
-    <div>
+    <div className="all">
       <Header />
       <h1>Contact</h1>
-      <a href="https://github.com/mireezeidan">
-        <img className="git-img" src={Github} alt="Github Logo" />
-      </a>
+      <div className="contact-div">
+        <div className="single-contact">
+          <Link to="https://github.com/mireezeidan">
+            <img className="contact-img" src={Github} alt="Github Logo" />
+          </Link>
+        </div>
+        <div className="resume-link">
+          <Link to="https://docs.google.com/document/d/1jP70hO0EyeLBYo_QQunGvu-jvXgVbKuPAPo0LaSPN_g/edit?usp=sharing">
+            <img className="contact-img" src={Resume} alt="Resume" />
+          </Link>
+          <p className="resume-text">Miree's Resume</p>
+        </div>
+        <div className="single-contact">
+          <Link to="https:www.linkedin.com/in/miree-zeidan">
+            <img className="contact-img" src={Linkedin} alt="LinkedIn Logo" />
+          </Link>
+        </div>
+      </div>
       <Footer />
     </div>
   );
